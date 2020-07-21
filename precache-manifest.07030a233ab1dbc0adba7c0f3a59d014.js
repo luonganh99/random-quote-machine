88cd59c686632bbc3caa834812b4c08c2901fed2
luonganh99 <nguyenhuuluonganh@gmail.com>
2020-07-21 12:36:58 +0700

    Updates

---
+++ precache-manifest.07030a233ab1dbc0adba7c0f3a59d014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57eaf4e5546be1323a83306d4c8af22",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "493f8bb9d8d06eea434c",
+    "url": "/random-quote-machine/static/css/main.1cbcbf02.chunk.css"
+  },
+  {
+    "revision": "1a8f63aaf3381a5542be",
+    "url": "/random-quote-machine/static/js/2.ffc87507.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.ffc87507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493f8bb9d8d06eea434c",
+    "url": "/random-quote-machine/static/js/main.fa9b3c29.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
